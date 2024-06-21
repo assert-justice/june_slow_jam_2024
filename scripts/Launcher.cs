452080e41ba75dbb0c89650e0b527b3aaf7d31ca
Riley <waskerdu@gmail.com>
2024-06-21 10:17:30 -0700

    launcher update

--- scripts/Launcher.cs
+++ scripts/Launcher.cs
@@ -3,6 +3,8 @@ using System;
 
 public partial class Launcher : Area2D
 {
+	[Export] public float Power = 600.0f;
+	[Export] public float LockoutTime = 0.2f;
 	AnimatedSprite2D sprite;
 	AudioStreamPlayer2D sound;
 	public override void _Ready()
@@ -13,7 +15,13 @@ public partial class Launcher : Area2D
 	private void _on_body_entered(Node2D body)
 	{
 		if(body is Player player){
-			player.SetVelocity(Vector2.Up * 600.0f + Vector2.Right * player.Velocity.X);
+			// convert player v into local coordinates
+			var vel = player.Velocity;
+			vel.Y = 0;
+			vel = Transform.BasisXformInv(vel);
+			vel.Y = -Power;
+			vel = Transform.BasisXform(vel);
+			player.SetVelocity(vel, LockoutTime);
 			sprite.Play();
 			sound.Play();
 		}
