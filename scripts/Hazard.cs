07e7343035992606c78a5e21a01e0bdce0dc27a5
Riley <waskerdu@gmail.com>
2024-06-22 09:15:23 -0700

    hazard has sprite, jiggles

--- scripts/Hazard.cs
+++ scripts/Hazard.cs
@@ -3,10 +3,29 @@ using System;
 
 public partial class Hazard : Area2D
 {
+	[Export] public float JiggleTime = 0.1f;
+	[Export] public float JiggleDistance = 2.0f;
+	Vector2 origin;
+	float jiggleClock = 0.0f;
+	AnimatedSprite2D sprite;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		GetNode<AnimatedSprite2D>("Sprite").Play();
+		sprite = GetNode<AnimatedSprite2D>("Sprite");
+		origin = Position;
+	}
+	public override void _PhysicsProcess(double delta)
+	{
+		if(jiggleClock > 0){
+			jiggleClock -= (float)delta;
+		}
+		else{
+			var offset = Vector2.Zero;
+			offset.X = GD.Randf() * 2 - 0.5f;
+			offset.Y = GD.Randf() * 2 - 0.5f;
+			Position = origin + offset * JiggleDistance;
+			jiggleClock = JiggleTime;
+		}
 	}
 	private void _on_body_entered(Node2D body)
 	{
