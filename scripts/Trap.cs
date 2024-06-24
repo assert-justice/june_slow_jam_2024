5ee95b1f7f04fa1bdd3b34075a0214e6b245c86a
Riley <waskerdu@gmail.com>
2024-06-23 18:38:52 -0700

    add trap sprites

--- scripts/Trap.cs
+++ scripts/Trap.cs
@@ -7,18 +7,23 @@ public partial class Trap : Node2D
 	[Export] public float BulletSpeed = 200.0f;
 	[Export] public float Cooldown = 3;
 	RayCast2D ray;
+	AnimatedSprite2D sprite;
 	float clock = 0.0f;
+	int lastFrame = 0;
 	// bool canFire = true;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		ray = GetNode<RayCast2D>("RayCast2D");
+		sprite = GetNode<AnimatedSprite2D>("Sprite");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _PhysicsProcess(double delta)
 	{
 		// if(!canFire) return;
+		if(sprite.Frame == 10 && lastFrame != 10) Fire();
+		lastFrame = sprite.Frame;
 		if(clock > 0) {
 			clock -= (float)delta;
 			return;
@@ -27,15 +32,18 @@ public partial class Trap : Node2D
 		// GD.Print(collider);
 		if(collider is CharacterBody2D){
 			// Shoot bullet
-			var bullet = BulletScene.Instantiate<Bullet>();
-			GetParent().AddChild(bullet);
-			bullet.Position = Position;
-			bullet.Rotation = Rotation;
-			bullet.Velocity = Transform.BasisXform(Vector2.Up * BulletSpeed);
+			sprite.Play();
 			// bullet.Velocity = Vector2.Up * BulletSpeed;
 			// canFire = false;
 			clock = Cooldown;
 			// GD.Print("fire!");
 		}
+	}
+	void Fire(){
+		var bullet = BulletScene.Instantiate<Bullet>();
+		GetParent().AddChild(bullet);
+		bullet.Position = Position;
+		bullet.Rotation = Rotation;
+		bullet.Velocity = Transform.BasisXform(Vector2.Up * BulletSpeed);
 	}
 }
